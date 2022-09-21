96b28273b0b1c852a4afe274e5b050b103c9a0b8
Katie Danger | GamerGirlGameboyMods <katie.danger.schmitz@gmail.com>
2022-09-21 11:39:46 -0500

    Fixed js prevent default behavior on search

--- src/app.js
+++ src/app.js
@@ -108,7 +108,7 @@ function search(city) {
 }
 
 function handleSubmit(event) {
-  event.preventDefault;
+  event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
